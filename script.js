2aa9ffcf6d3d39ba7fc246e9069b8b3916114729
RyanTjia19 <ryan.tjia19@my.stjohns.edu>
2022-10-26 13:10:17 -0400

    Added a simple function that shows the questions and answers Added a way to randomize the questions Need to add more questions Need to add the functionality of picking the chocies and seeing if they are correct

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-var render_view = (viewId, modelIndex) => {
+
+var simple = {};
+var test = ["hi"];
+var set;
+
+//Gets the correct data for the correct quiz
+//For now, it'll be one quiz so there will be no parameter
+var GetData = () => {
+	fetch("https://my-json-server.typicode.com/RyanTjia19/QuizQuestions/Simple")
+	.then((response) => {
+		return response.json();
+	})
+	.then((results) => {
+		simple = results;
+		set = [simple, test];
+	})
+}
+
+var render_view = (viewId) => {
+
+	//Have access to the template used
 	var source = document.querySelector(viewId).innerHTML;
 	var template = Handlebars.compile(source);
-	var html = template(model[modelIndex]);
+
+	//Randomize the question
+	var typeRandom = Math.floor(Math.random() * set.length);
+	var chosenType = set[typeRandom];
+
+	var questionRandom = Math.floor(Math.random() * chosenType.length);
+	var html = template(chosenType[questionRandom]);
 
 	document.querySelector("#view_template").innerHTML = html;
+	console.log(chosenType);
 }
