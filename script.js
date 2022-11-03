80254a27ecb3f747f3bd7e538f1015ec75c8be8f
RyanTjia19 <ryan.tjia19@my.stjohns.edu>
2022-11-03 18:40:57 -0400

    Completed the quiz * Not enough space to add more questions to the database, need more repositories * The reaction response is centered, but not vertically * Only one quiz

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//json object to keep track of the available quizzes
+//There will be only one quiz for now
+var Quizzes = {
+	"Quiz" : [
+		{"Subject" : "ComputerScience"}
+	]
+}
+
 //Json object to keep track of question count
 var TitleQuiz = {
 	"name": "",
@@ -13,20 +21,46 @@ var choice = -1;
 var set = [];
 var chosenQuestion;
 
-document.addEventListener("DOMContentLoaded", ()=> {
+//Loads the main menu once the page is ready
+window.onload = () => {
+
+	var source = document.querySelector('#Menu').innerHTML;
+	var template = Handlebars.compile(source);
+	var html = template(Quizzes);
+
+	document.querySelector("#view_template").innerHTML = html;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
 
 	//This is event delegation, any child member of "view_template" will trigger this
 	document.querySelector("#view_template").onclick = (e) => {
 		handle_event(e);
-	}
+	};
 })
 
 function handle_event(e) {
 
+	//For when user is starting the quiz
+	//Checks if this button belongs in <div> with an id of "quizzes"
+	if (e.target.offsetParent.id == "quizzes") {
+		
+		//Uses the id of the button to get the correct data
+		GetData(e.target.id);
+	}
+
 	//Checks for multiple choices (their id is a number)
 	//Store the user's answer
-	if (parseInt(e.target.id) >= 0) {
+	else if (parseInt(e.target.id) >= 0) {
+
+		//Change the button color to the default color, to show a different choice was picked
+		//But only if the user have made a choice before
+		if (choice >= 0) {
+			document.getElementById(choice).style.backgroundColor = "white";
+		}
+
 		choice = e.target.id;
+		document.getElementById(choice).style.backgroundColor = "yellow";
 	}
 
 	//Submit button and resets the user's choice
@@ -59,9 +93,22 @@ function handle_event(e) {
 	else if (e.target.id == "understood") {
 		updatePage();
 	}
+
+	//Returns to the home page
+	else if (e.target.id == "homePage") {
+		var source = document.querySelector('#Menu').innerHTML;
+		var template = Handlebars.compile(source);
+		var html = template(Quizzes);
+
+		document.querySelector("#view_template").innerHTML = html;
+	}
 }
 
 //Gets the correct data for the correct quiz
+//If we were to use multiple repositories, where each repository contain questions for ONE quiz,
+//then the url will be something like
+//https://my-json-server.typicode.com/RyanTjia19/'quiz'/questions
+//Where 'quiz' will be the name of the quiz that the user wants to take
 var GetData = (quiz) => {
 	fetch(`https://my-json-server.typicode.com/RyanTjia19/QuizQuestions/${quiz}`)
 	.then((response) => {
@@ -220,6 +267,6 @@ function checkAnswer(number) {
 	var html = template(chosenQuestion);
 
 	//Changing the template and their css
-	document.querySelector(".background").className += ' overlapBackground';
+	document.querySelector(".Background").style.width = "100%";
 	document.querySelector("#response").innerHTML = html;
 }
